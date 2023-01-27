3a1a58a5b81e2db1e624449ecf7e1d0ce264f709
Juan Daniel <juan.arce@sofka.com.co>
2023-01-27 14:25:13 -0500

    Firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'pokemon-88c81',
+    appId: '1:871681354173:web:d2b47d25742b9d52fd4ad4',
+    storageBucket: 'pokemon-88c81.appspot.com',
+    apiKey: 'AIzaSyDsyRen0Sgi2CJez33Yx-95v54evr8x9IE',
+    authDomain: 'pokemon-88c81.firebaseapp.com',
+    messagingSenderId: '871681354173',
+  },
   production: true
 };
