b932b1cdfb9b40c495589ceec8dbd00e5cf83cd4
Juan Daniel <juan.arce@sofka.com.co>
2023-02-14 07:39:59 -0500

    Page to hold all user data

--- src/app/modules/pages/user-dashboard/user-dashboard.component.ts
+++ src/app/modules/pages/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 import { AuthService } from '../../auth/services/auth.service';
 import { User } from '../../core/domain/entities/user.model';
 import { UserService } from '../../core/services/user.service';
@@ -11,13 +12,50 @@ import { UserService } from '../../core/services/user.service';
 export class UserDashboardComponent implements OnInit {
 	user!: User;
 
+	@ViewChild('right')
+	rightAlert!: SwalComponent;
+
+	@ViewChild('wrong')
+	wrongAlert!: SwalComponent;
+
+	@ViewChild('rechargeSwal')
+	alert!: SwalComponent;
+
 	constructor(
 		private readonly _userService: UserService,
 		private readonly _authService: AuthService,
-	) {}
+	) { }
 
 	async ngOnInit() {
 		const currentUser = this._authService.getCurrentUser();
-		this.user = (await this._userService.findUser(currentUser!.uid!))!;
+		this._userService.streamToUser(currentUser!.uid!).subscribe((u) => { this.user = u })
 	}
+
+
+	async recharge(amount: string) {
+		const uid = this._authService.getCurrentUser()?.uid;
+
+		if (!uid) return;
+
+		const response = await this._userService.recharge(uid, parseInt(amount));
+
+
+
+
+		if (!response.success) {
+			this.wrongAlert.title = "Something went wrong";
+			this.wrongAlert.text = response.message;
+
+			this.wrongAlert.fire();
+
+			return;
+		}
+
+		this.rightAlert.title = "Success";
+		this.rightAlert.text = response.message;
+
+
+		this.rightAlert.fire();
+	}
+
 }
